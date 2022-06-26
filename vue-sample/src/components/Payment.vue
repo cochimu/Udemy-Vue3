adef96026e1f77b80238dcc1f0934bd7323bd473
cochimu <cochimu1221@icloud.com>
2022-06-26 19:43:07 +0900

    20

--- vue-sample/src/components/Payment.vue
+++ vue-sample/src/components/Payment.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
+const itemName1 = 'Desk'
+const itemName2 = 'Bike'
+
+const price1 = 40000
+const price2 = 20000
+
+const url1 = ''
 </script>
 
 <template>
 	<div class="container">
 		<h1>支出</h1>
 		<div class="payment">
-			<label>テスト</label>
-			<label>500円</label>
+			<label>{{ itemName1 }}</label>
+			<label>{{ price1 }} yen</label>
+			<!-- v-bindで動的な値を入れられる -->
+			<a v-bind:href="url1">購入</a>
+		</div>
+		<div class="payment">
+			<label>{{ itemName2 }}</label>
+			<label>{{ price2 }} yen</label>
+			<a href="">購入</a>
 		</div>
 	</div>
 </template>
@@ -25,6 +39,7 @@
 	height: 80px;
 	width: 400px;
 	background-color: aliceblue;
+	margin-bottom: 8px;
 }
 
 label {
