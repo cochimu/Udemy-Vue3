14ec892e1f29d824050ea11d0bba9a88424f3b91
cochimu <cochimu1221@icloud.com>
2022-07-03 16:01:55 +0900

    25

--- vue-sample/src/components/Payment.vue
+++ vue-sample/src/components/Payment.vue
@@ -1,58 +1,77 @@
 <script setup lang="ts">
 // リアクティブな値とは中身が変わったら再度templateを読み込みしなおしてね、という値
 // それをするにはref形式にする。そのためにimportする
-// import { ref } from 'vue'
-
 // オフジェクトそのものをリアクティブにしたい場合はrefじゃなくてreactiveを使う
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
 
 // このようにref形式にしておけばここの中身が変わればtemplateを再度読み込みしてね、という合図になる
 // const itemName1 = ref<string>('Desk')
-const itemName2 = 'Bike'
 
+// オブジェクトそのものをリアクティブにしたいときはこうなる
 const item1 = reactive ({
 	name: 'Desk',
 	price: 40000
 })
 
-// const price1 = 40000
+const itemName2 = 'Bike'
 const price2 = 20000
-
 const url1 = ''
 
 const buy = (itemName: string) => {
 	alert('Are you sure buy ' + itemName + '?')
 }
 
-const input = (event: any) => {
-	// eventの中のtargetの中のvalueというオブジェクトを見てる
-	// inputで入力した値がここに入ってるから
-	console.log('event:', event.target.value)
-	// itemName1にinputで入れた値を代入する
-	// refに代入するときは.valueで再代入する。constだけどオブジェクトの中のvalueへの代入だから再代入できる
-	// itemName1.value = event.target.value
+// const input = (event: any) => {
+// 	// eventの中のtargetの中のvalueというオブジェクトを見てる
+// 	// inputで入力した値がここに入ってるから
+// 	console.log('event:', event.target.value)
+// 	// itemName1にinputで入れた値を代入する
+// 	// refに代入するときは.valueで再代入する。constだけどオブジェクトの中のvalueへの代入だから再代入できる
+// 	// itemName1.value = event.target.value
 
-	// オブジェクトに代入するときはこの書き方
-	item1.name = event.target.value
+// 	// オブジェクトに代入するときはこの書き方
+// 	item1.name = event.target.value
+// }
+
+// const inputPrice = (event: any) => {
+// 	item1.price = event.target.value
+// }
+
+const clear = () => {
+	item1.name = '',
+	item1.price = 0
 }
 
-const inputPrice = (event: any) => {
-	item1.price = event.target.value
-}
+const budget = 50000
 
+// 何らかの条件をもとに値を生成する時にはcomputedを使う
+const priceLabel = computed(() => {
+	if (item1.price > budget * 2) {
+		return 'too expensive...'
+	} else if  (item1.price > budget) {
+		return 'expensive...'
+	} else {
+		return item1.price + 'yen'
+	}
+})
 
 </script>
 
 <template>
 	<div class="container">
 		<h1>支出</h1>
-
-		<input v-on:input="input"/>
-		<input v-on:input="inputPrice"/>
+		<!-- 双方向にバインドするときはv-model -->
+		<input v-model="item1.name" />
+		<!-- <input v-on:input="input" v-bind:value="item1.name" /> -->
+		<input v-model="item1.price" />
+		<!-- <input v-on:input="inputPrice" v-bind:value="item1.price" /> -->
+		
+		<button v-on:click="clear">Clear</button>
 
 		<div class="payment">
 			<label>{{ item1.name }}</label>
-			<label>{{ item1.price }} yen</label>
+			<label> {{ priceLabel }}</label>
+			<!-- <label>{{ item1.price }} yen</label> -->
 			<!-- v-bindで変数などの動的な値を入れられる -->
 			<a v-bind:href="url1">購入</a>
 			<!-- v-onでメソッドを埋め込める -->
