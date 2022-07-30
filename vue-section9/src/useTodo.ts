55dee2a636f3ce6c471d26e7c0cb99a88d248006
cochimu <cochimu1221@icloud.com>
2022-07-30 15:52:00 +0900

    InjectionKey

--- vue-section9/src/useTodo.ts
+++ vue-section9/src/useTodo.ts
@@ -1,21 +1,11 @@
 
-import { ref } from 'vue'
+import {InjectionKey, ref} from 'vue'
 type Todo = {
     id: number,
     title: string
 }
 
 const defaultTodos = [{id: 0, title: 'first'}, {id: 1, title: 'second'}]
-// export const todos = ref<Todo[]>(defaultTodos)
-//
-// export const addTodo = (title: string) => {
-//     const newTodo: Todo = {
-//         id: Math.random(),
-//         title,
-//     }
-//     todos.value.push(newTodo)
-// }
-
 export const todos = (() => {
     const todos = ref<Todo[]>(defaultTodos)
     const addTodo = (title: string) => {
@@ -28,3 +18,6 @@ export const todos = (() => {
 
     return { todos, addTodo }
 })()
+
+type TodosType = typeof todos
+export const todoKey: InjectionKey<TodosType> = Symbol('useTodos')
